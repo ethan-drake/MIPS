eff977fb03ecf8bf7c05a5658ef4ef7b7496a9d3
David Michel <david.michelt@iteso.mx>
2023-04-20 20:31:21 -0600

    added forwardB condition

--- src/forward_unit.v
+++ src/forward_unit.v
@@ -8,15 +8,19 @@
 module forward_unit (
 	//Inputs
 	input ex_mem_regWrite,
-	input [4:0] ex_mem_rd, id_ex_reg_rs1,
+	input [4:0] ex_mem_rd, id_ex_reg_rs1, id_ex_reg_rs2,
 	//Outputs
-	output reg [1:0] forwardA
+	output reg [1:0] forwardA, forwardB
 );
 
 //EX Forward Unit
 always @(*) begin
+	//EX forward UNIT 1st
 	if ((ex_mem_regWrite) && (ex_mem_rd != 0) && (ex_mem_rd == id_ex_reg_rs1)) begin
 		assign forwardA = 2'b10;
+	end
+	if ((ex_mem_regWrite) && (ex_mem_rd != 0) && (ex_mem_rd == id_ex_reg_rs2)) begin
+		assign forwardB = 2'b10;
 	end
 end
 	
