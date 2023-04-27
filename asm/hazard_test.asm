2a3f97dfb2a36c9763d412958c76b982d156a8f4
Eduardo Ethandrake Castillo Pulido <eduardo_ethandrake@yahoo.com.mx>
2023-04-26 22:10:51 -0600

    modified hazard test code

--- asm/hazard_test.asm
+++ asm/hazard_test.asm
@@ -1,13 +1,17 @@
 .text
 main:
+	auipc s2,    0x0000fc10 #cargar valor base de 10010024
+	addi s2, s2, 0x24
 lw s1, 4(s2)
 add s4,s1,s5
 and s6,s1,s4
+add s6, zero, s2
+sw s6, 4(s6)
 lw s1, 4(s6)
 lw s2, 4(s1)
 addi s9,s2,0x9
 addi s4,s9,0x4
 add s5,s9,s2
-lw s5, 4(s5)
+lw s5, 4(s2)
 add s5,s1,s5
 add s5,s5,s5
