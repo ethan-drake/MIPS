9a33bfb6ba3c120fd21aab10e9fbbb24c7594e0a
David Michel <david.michelt@iteso.mx>
2023-04-26 20:38:17 -0600

    added a more compelx program to test fwd unit and dependecies

--- asm/fwd_unit_test.asm
+++ asm/fwd_unit_test.asm
@@ -1,6 +1,17 @@
 .text
 main:
-addi s0, s1, 5 
-add s2, s0, s1 
-addi s3, s0, 15 
-add s4, s2, s1 
+addi s2,s2,0x3
+addi s5,s5,0x5
+add s1,s2,s2
+sub s4,s1,s5
+and s6,s1,s4
+or  s8,s1,s4
+xor s4,s1,s5
+addi s4,s8,0x9
+addi s6,s8,0x2
+addi s4,s8,0x4
+add s5,s5,s2
+slt s5,s5,s4
+or s3,s1,s5
+add s5,s5,s5
+sll s7,s5,s3
