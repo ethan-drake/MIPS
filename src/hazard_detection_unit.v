6f7b4bf4db510953b20f5851285af31fea2e4f1d
Eduardo Ethandrake Castillo Pulido <eduardo_ethandrake@yahoo.com.mx>
2023-04-26 21:43:43 -0600

    hazar module added

---
+++ src/hazard_detection_unit.v
@@ -0,0 +1,17 @@
+module hazard_detection_unit(
+    input id_ex_memread,
+    input [4:0] id_ex_rd,
+    input [4:0] if_id_rs1,
+    input [4:0] if_id_rs2,
+    output pc_stall,
+    output if_id_stall,
+    output stall_mux
+);
+
+
+
+
+
+
+
+endmodule
