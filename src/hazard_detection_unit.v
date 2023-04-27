b2ecf68e514a5e49d7b998d02c3f1b419643a8cc
Eduardo Ethandrake Castillo Pulido <eduardo_ethandrake@yahoo.com.mx>
2023-04-26 21:47:41 -0600

    hazard detection added

--- src/hazard_detection_unit.v
+++ src/hazard_detection_unit.v
@@ -3,11 +3,23 @@ module hazard_detection_unit(
     input [4:0] id_ex_rd,
     input [4:0] if_id_rs1,
     input [4:0] if_id_rs2,
-    output pc_stall,
-    output if_id_stall,
-    output stall_mux
+    output reg pc_stall,
+    output reg if_id_stall,
+    output reg stall_mux
 );
 
+always@(*) begin
+    pc_stall=1'b0;
+    if_id_stall=1'b0;
+    stall_mux=1'b0;
+    if(id_ex_memread && ((id_ex_rd == if_id_rs1) || (id_ex_rd == if_id_rs2)))
+    begin
+        pc_stall=1'b1;
+        if_id_stall=1'b1;
+        stall_mux=1'b1;
+    end
+
+end
 
 
 
