c902f2be08d8a3a31f6067b860b518ff546616e6
Eduardo Ethandrake Castillo Pulido <eduardo_ethandrake@yahoo.com.mx>
2023-05-10 19:59:03 -0600

    changed mem to reg BHB

--- src/branch_prediction.v
+++ src/branch_prediction.v
@@ -19,9 +19,36 @@ module branch_prediction #(parameter DATA_WIDTH = 32,parameter BRANCH_NO=8)(
     output prediction_checkout_ex_mem
 );
 
+//Declare our memory
+//reg [31:0] registers [0:31];
+localparam B_TYPE = 7'b1100011;
 reg [DATA_WIDTH-1:0] BHB [(BRANCH_NO)-1:0];
 reg [BRANCH_NO-1:0] BHT = {BRANCH_NO{1'b0}};
-localparam B_TYPE = 7'b1100011;
+
+generate
+	genvar i;
+	for(i=0;i<BRANCH_NO;i=i+1) begin : test
+        always@(posedge i_clk, negedge i_rst_n) begin
+            if(!i_rst_n)
+                BHB[i] <= 32'h0;
+            else if(ex_mem_opcode==B_TYPE && ex_mem_pc[2:0]==i)
+                BHB[i] <= ex_mem_branch_target;
+            else
+                BHB[i] <= BHB[i];
+        end
+	end
+endgenerate
+
+always @(posedge i_clk)
+begin
+	//Write
+	if (ex_mem_opcode==B_TYPE) begin
+		BHT[ex_mem_pc[2:0]] <= ex_mem_branch_taken;
+    end
+end
+
+/*
+
 
 integer i;
 initial begin
@@ -37,7 +64,7 @@ begin
         BHB[ex_mem_pc[2:0]] <= ex_mem_branch_target;
     end
 end
-	
+	*/
 // Reading if memory read enable
 assign prediction = (if_id_opcode == B_TYPE) ? BHT[if_pc[2:0]] : 1'b0;
 assign branch_target = (if_id_opcode == B_TYPE) ? BHB[if_pc[2:0]] : 32'b0;
