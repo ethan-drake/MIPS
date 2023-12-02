10b030632f1a0141e21a52916fe755542c469809
David Michel <david.michel010@gmail.com>
2023-12-02 05:54:08 +0000

    final_v2

--- synthesis/sim/src/README.txt
+++
@@ -1,3 +0,0 @@
-Verilog standard cells libs location
-
-/CMC/kits/cadence/GPDK045/gsclib045_all_v4.4/gsclib045/verilog
--- synthesis/sim/src/xcelium/probes.tcl
+++
@@ -1,8 +0,0 @@
-set db_name "sim_database"
-set db_folder_name $db_name
-append db_folder_name ".smh"
-set top_name "risc_v_top_tb"
-
-database -open $db_name -into $db_folder_name -event -default
-probe -create $top_name -depth all -tasks -functions -all -database $db_name
-run
